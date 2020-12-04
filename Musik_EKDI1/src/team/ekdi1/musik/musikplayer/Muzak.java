6756f7584bc48ea188452d0cb02d68d5a81263b9
GigaLi <andrejlich1@gmail.com>
2020-12-04 15:56:22 +0100

    Muzik Klasse Anpassung

--- Musik_EKDI1/src/team/ekdi1/musik/musikplayer/Muzak.java
+++ Musik_EKDI1/src/team/ekdi1/musik/musikplayer/Muzak.java
@@ -32,35 +32,49 @@ public class Muzak /*implements LineListener*/ {
      * Play a given audio file.
      * @param audioFilePath Path of the audio file.
      */
-    public void play(String audioFilePath) {
-	File audioFile = new File(audioFilePath);
+    public void play(String audioFilePath1, String audioFilePath2,String audioFilePath3, String audioFilePath4) {
+	File audioFile1 = new File(audioFilePath1);
+	File audioFile2 = new File(audioFilePath2);
+	File audioFile3 = new File(audioFilePath3);
+	File audioFile4 = new File(audioFilePath4);
 	
 	try {
-	    AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-	    AudioInputStream audioStream2 = AudioSystem.getAudioInputStream(audioFile);	    
-	    AudioFormat format = audioStream.getFormat();
+	    AudioInputStream audioStream1 = AudioSystem.getAudioInputStream(audioFile1);
+	    AudioInputStream audioStream2 = AudioSystem.getAudioInputStream(audioFile2);	 
+	    AudioInputStream audioStream3 = AudioSystem.getAudioInputStream(audioFile3);
+	    AudioInputStream audioStream4 = AudioSystem.getAudioInputStream(audioFile4); 
+	    
+	    AudioFormat format1 = audioStream1.getFormat();
 	    AudioFormat format2 = audioStream2.getFormat();
+	    AudioFormat format3 = audioStream3.getFormat();
+	    AudioFormat format4 = audioStream4.getFormat();
 	    
-	    DataLine.Info info = new DataLine.Info(Clip.class, format);
+	    DataLine.Info info1 = new DataLine.Info(Clip.class, format1);
 	    DataLine.Info info2 = new DataLine.Info(Clip.class, format2);
+	    DataLine.Info info3 = new DataLine.Info(Clip.class, format3);
+	    DataLine.Info info4 = new DataLine.Info(Clip.class, format4);
 	    
-	    Clip audioClip = (Clip) AudioSystem.getLine(info);
+	    Clip audioClip1 = (Clip) AudioSystem.getLine(info1);
 	    Clip audioClip2 = (Clip) AudioSystem.getLine(info2);
+	    Clip audioClip3 = (Clip) AudioSystem.getLine(info3);
+	    Clip audioClip4 = (Clip) AudioSystem.getLine(info4);
 	    
 	    //audioClip.addLineListener(this);
 	    
-	    audioClip.open(audioStream);
+	    audioClip1.open(audioStream1);
 	    audioClip2.open(audioStream2);
+	    audioClip3.open(audioStream3);
+	    audioClip4.open(audioStream4);
 	    
-	    audioClip.start();
-
-	    
+	    audioClip1.start();
+	    audioClip2.start();
+	    audioClip3.start();
+	    audioClip4.start();
+	   	    
 	    while (!playCompleted) {
 		// wait for the playback completes
 		try {
-		    Thread.sleep(50);
-		    //audioClip2.start();
-		    Thread.sleep(1000);
+		    Thread.sleep(1500);
 		    playCompleted = true;
 		    //audioClip2.start();
 		} catch (InterruptedException ex) {
@@ -68,8 +82,11 @@ public class Muzak /*implements LineListener*/ {
 		}
 		}
 	    
-	    audioClip.close();
+	    audioClip1.close();
 	    audioClip2.close();
+	    audioClip3.close();
+	    audioClip4.close();
+
 
 	    
 	} catch (UnsupportedAudioFileException ex) {
@@ -103,18 +120,18 @@ public class Muzak /*implements LineListener*/ {
 	} 
     */
     public static void main(String[] args) {
-	String audioFilePath = "C:\\Users\\jsste\\Downloads\\anhang(1)\\anhang\\ton.wav";
-	String audioFilePath2 = "C:\\Users\\jsste\\Downloads\\anhang(1)\\anhang\\tonauswahl\\Bamboo.wav";
-	//Muzak player = new Muzak();
-	Muzak player2 = new Muzak();
-	for(int i = 0; i < 10000; i++) {
-		Muzak player = new Muzak();
-		player.play(audioFilePath);
-		
-		
-	}
-	
-	
-	player2.play(audioFilePath2);
+//	String audioFilePath = "C:\\Users\\jsste\\Downloads\\anhang(1)\\anhang\\ton.wav";
+//	String audioFilePath2 = "C:\\Users\\jsste\\Downloads\\anhang(1)\\anhang\\tonauswahl\\Bamboo.wav";
+//	//Muzak player = new Muzak();
+//	Muzak player2 = new Muzak();
+//	for(int i = 0; i < 10000; i++) {
+//		Muzak player = new Muzak();
+//		player.play(audioFilePath);
+//		
+//		
+//	}
+//	
+//	
+//	player2.play(audioFilePath2);
     }   
 }
