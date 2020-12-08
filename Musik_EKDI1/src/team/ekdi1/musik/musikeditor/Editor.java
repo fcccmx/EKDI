acbecd01222ab1a45aad9b67756297a1e62f40d9
fcccmx <cmxfeng@outlook.com>
2020-12-08 19:33:22 +0800

    Musik editieren

--- Musik_EKDI1/src/team/ekdi1/musik/musikeditor/Editor.java
+++ Musik_EKDI1/src/team/ekdi1/musik/musikeditor/Editor.java
@@ -1,5 +1,29 @@
 package team.ekdi1.musik.musikeditor;
-
+import team.ekdi1.musik.musikplayer.LadeDatei;
 public class Editor {
-
+//		public static void main(String[] args) {
+//			
+//	
+//		}
+		public static void Add(int takt,int Kanal,String Tone,String csvPath) {
+			LadeDatei editor= new LadeDatei();
+			String KanalArray[][]=editor.csvRead(csvPath);
+			
+			//editor.csvWrite(KanalArray);
+			
+		}
+		
+		public void Replace(int takt,int Kanal,String Tone,String csvPath) {
+			LadeDatei editor= new LadeDatei();
+			String KanalArray[][]=editor.csvRead(csvPath);
+			KanalArray[takt][Kanal]=Tone;
+			//editor.csvWrite(KanalArray);
+			
+		}
+		
+		public void Delete(int takt,int Kanal,String csvPath) {
+			LadeDatei editor= new LadeDatei();
+			String KanalArray[][]=editor.csvRead(csvPath);
+			KanalArray[takt][Kanal]=" ";
+		}
 }
