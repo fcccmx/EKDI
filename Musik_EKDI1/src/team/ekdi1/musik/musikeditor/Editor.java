04dfa2a86592b703cf5dfb9573e005cbe1dd8bc5
fcccmx <cmxfeng@outlook.com>
2020-12-10 17:41:32 +0800

    Completes the method in Editor

--- Musik_EKDI1/src/team/ekdi1/musik/musikeditor/Editor.java
+++ Musik_EKDI1/src/team/ekdi1/musik/musikeditor/Editor.java
@@ -9,22 +9,26 @@ public class Editor {
 		LadeDatei editor= new LadeDatei();
 		this.MusikArray=editor.csvRead(csvPath);
 	}
-		public void Add(int takt,int Kanal,String Tone) {
-			this.MusikArray.add(takt*Kanal-1,Tone);
-			//editor.csvWrite(KanalArray);			
-		}
+	
+	public void Add(int takt,int Kanal,String Tone) {
+		this.MusikArray.add(takt*Kanal-1,Tone);
+		//editor.csvWrite(KanalArray);			
+	}
 		
-		public void Replace(int takt,int Kanal,String Tone) {
-			this.MusikArray.set(takt*Kanal-1, Tone);
+	public void Replace(int takt,int Kanal,String Tone) {
+		this.MusikArray.set(takt*Kanal-1, Tone);
 			
-			//editor.csvWrite(KanalArray);
+		//editor.csvWrite(KanalArray);
 			
-		}
+	}
 		
-		public void Delete(int takt,int Kanal) {
-			this.MusikArray.set(takt*Kanal-1, "");
+	public void Delete(int takt,int Kanal) {
+		this.MusikArray.set(takt*Kanal-1, "");
 			
-		}
-		
+	}
+	
+//		public static void main(String[] args) {
+//			
+//		}
 		
 }
