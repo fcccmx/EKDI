8505eeba34a9ba33f7b1c3f79aa2479657cc7ab4
fcccmx <cmxfeng@outlook.com>
2020-12-11 21:43:43 +0800

    1

--- Musik_EKDI1/src/team/ekdi1/musik/musikeditor/Editor.java
+++ Musik_EKDI1/src/team/ekdi1/musik/musikeditor/Editor.java
@@ -2,6 +2,7 @@ package team.ekdi1.musik.musikeditor;
 import java.util.ArrayList;
 
 import team.ekdi1.musik.musikplayer.LadeDatei;
+
 public class Editor {
 	private ArrayList<String> MusikArray=new ArrayList<String>();
 	
@@ -28,7 +29,13 @@ public class Editor {
 	}
 	
 //		public static void main(String[] args) {
-//			
+//			Editor test1=new Editor("C:\\Users\\Ïº½È\\Desktop\\example.csv");
+//			System.out.print(test1.MusikArray);
+//			System.out.println();
+//			test1.Replace(3, 6, "F4");
+//			System.out.print(test1.MusikArray);
+//			LadeDatei test2=new LadeDatei();
+//			test2.csvWrite(test1.MusikArray,"C:\\Users\\Ïº½È\\Desktop\\e.csv");
 //		}
 		
 }
