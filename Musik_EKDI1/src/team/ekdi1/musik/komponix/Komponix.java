c8a39c9959fd5feba7be6de22ace7156c787141b
fcccmx <cmxfeng@outlook.com>
2020-12-23 19:54:36 +0800

    komponix

--- Musik_EKDI1/src/team/ekdi1/musik/komponix/Komponix.java
+++ Musik_EKDI1/src/team/ekdi1/musik/komponix/Komponix.java
@@ -1,5 +1,15 @@
 package team.ekdi1.musik.komponix;
 
+import java.util.ArrayList;
+
 public class Komponix {
+	
+	public ArrayList<String> Random(int takt) {
+		ArrayList<String> randomArray = new ArrayList<String>();
+		for (int i = 0; i < takt; i++) {
+			
+		}
+		return randomArray;
+	}
 
 }
