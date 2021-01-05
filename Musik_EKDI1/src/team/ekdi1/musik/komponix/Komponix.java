2fc85569638a3d4cff233cbb9c57e61a0d74c3b1
fcccmx <cmxfeng@outlook.com>
2021-01-05 23:47:28 +0800

    random generated score

--- Musik_EKDI1/src/team/ekdi1/musik/komponix/Komponix.java
+++ Musik_EKDI1/src/team/ekdi1/musik/komponix/Komponix.java
@@ -2,14 +2,26 @@ package team.ekdi1.musik.komponix;
 
 import java.util.ArrayList;
 
+import java.util.*;
+
 public class Komponix {
 	
-	public ArrayList<String> Random(int takt) {
+	public ArrayList<String> Randomarray(int takt) {
 		ArrayList<String> randomArray = new ArrayList<String>();
+		Random r=new Random();
 		for (int i = 0; i < takt; i++) {
-			
+			for (int j = 0; j < 4; j++) {
+				randomArray.add(Integer.toString(r.nextInt(46)+1));
+			}
 		}
 		return randomArray;
 	}
 
+	public static void main(String[] args){
+		ArrayList<String> rA = new ArrayList<String>();
+		Komponix t=new Komponix();
+		rA=t.Randomarray(20);
+		System.out.println(rA);
+		
+	}
 }
