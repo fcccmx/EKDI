328691b556333f2bdb2fda39805189854c6e93f9
fcccmx <cmxfeng@outlook.com>
2021-01-15 23:48:40 +0800

    Added the ability to control the status of music playback via text box

--- Musik_EKDI1/src/team/ekdi1/musik/main/MainFrame.java
+++ Musik_EKDI1/src/team/ekdi1/musik/main/MainFrame.java
@@ -33,6 +33,7 @@ import javax.swing.table.JTableHeader;
 import javax.swing.table.TableModel;
 
 import team.ekdi1.musik.musikplayer.LadeDatei;
+import team.ekdi1.musik.musikplayer.Musikspieler;
 
 
 public class MainFrame extends JFrame{
@@ -41,7 +42,10 @@ public class MainFrame extends JFrame{
 	JPanel panel1=new JPanel();
 	JScrollPane jScrollPanel= new JScrollPane();
 	JTextField playStatus=new JTextField("PlayStatus");
-	JLabel jL1_states=new JLabel();
+	JLabel jL1_states=new JLabel("P. Pause");
+	JLabel jL2=new JLabel("F. Fortsetzen");
+	JLabel jL3=new JLabel("V. Von vorne");
+	JLabel jL4=new JLabel("S. Stop");
 	JTable jT=new JTable();
 	JTableHeader jTableHeader1=jT.getTableHeader();
 	JButton jB1_openCSV=new JButton("csvFile-oeffnen");
@@ -49,12 +53,14 @@ public class MainFrame extends JFrame{
 	JButton jB3_editor=new JButton("Editor");
 	JButton jB4_kompositon=new JButton("kompositon");
 	JButton jB5_export=new JButton("MP3 exportieren");
+	JButton jB6_addline=new JButton("Eine Zeile hinzufügen");
+	JButton jB7_deleteline=new JButton("Eine Zeile löschen");
 	String[] columnNames= {"Kanal1","Kanal2","Kanal3","Kanal4"};
+	String[][] komplettArray=new String[64][4];
 	String csvFilePath;
     ArrayList<String> musikArray;
     String oldCellValue=null;
-//    Vector vcolumnNames=new Vector();
-//    Vector vData=new Vector();
+
 
     
 
@@ -65,6 +71,7 @@ public class MainFrame extends JFrame{
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -78,41 +85,48 @@ public class MainFrame extends JFrame{
         this.setBounds(50,50,800, 600);
 		panel1.setLayout(null);
 		
-		playStatus.setBounds(50,440,100,30);
-		jL1_states.setBounds(50, 470, 200, 30);
+		playStatus.setBounds(50,420,100,30);
+		playStatus.addActionListener(new TextField_PlayStatus_Listener());
+		jL1_states.setBounds(50, 450, 200, 30);
+		jL2.setBounds(50,470,200,30);
+		jL3.setBounds(50,490,200,30);
+		jL4.setBounds(50,510,200,30);
 		
-		jB1_openCSV.setBounds(600, 110, 150, 30);
+		jB1_openCSV.setBounds(600, 100, 150, 30);
 		jB1_openCSV.addActionListener(new jButton1_openCSVFile_Listener());
 		
 		jB2_storeCSV.setBounds(600, 180, 150, 30);
 		jB2_storeCSV.addActionListener(new JButton2_storeCSVFile_Listener());
 		
-		jB3_editor.setBounds(180, 440, 100, 30);
+		jB3_editor.setBounds(180, 420, 100, 30);
 		jB3_editor.addActionListener(new jButton3_editor_Listener());
+		jB6_addline.setBounds(210, 470, 180, 30);
+		jB7_deleteline.setBounds(210, 520, 180, 30);
 		
-		jB4_kompositon.setBounds(310,440, 100, 30);
+		jB4_kompositon.setBounds(310,420, 100, 30);
 		
-		jB5_export.setBounds(430, 440, 100, 30);
+		jB5_export.setBounds(430, 420, 150, 30);
+		
+		
 		
 		this.getContentPane().add(panel1,BorderLayout.CENTER);
 		panel1.add(playStatus);
 		panel1.add(jL1_states);
+		panel1.add(jL2);
+		panel1.add(jL3);
+		panel1.add(jL4);
 		panel1.add(jB1_openCSV);
 		panel1.add(jB2_storeCSV);
 		panel1.add(jB3_editor);
 		panel1.add(jB4_kompositon);
 		panel1.add(jB5_export);
+		panel1.add(jB6_addline);
+		panel1.add(jB7_deleteline);
 		
-//		vcolumnNames.add("Kanal1");
-//		vcolumnNames.add("Kanal2");
-//		vcolumnNames.add("Kanal3");
-//		vcolumnNames.add("Kanal4");
 		DefaultTableModel model=new DefaultTableModel(columnNames,0);
 		jT=new JTable(model);
-//		jT.addMouseListener(new mouseClick_Listener());
-//		jT.getModel().addTableModelListener(new tablecChanged_Listener());
 		jScrollPanel.getViewport().add(jT);
-		jScrollPanel.setBounds(30, 100, 500, 300);
+		jScrollPanel.setBounds(50, 80, 500, 300);
 		panel1.add(jScrollPanel);
 		
 			
@@ -135,13 +149,15 @@ public class MainFrame extends JFrame{
 			if (returnVal==JFileChooser.APPROVE_OPTION) {
 				csvFilePath=chooser.getSelectedFile().getAbsolutePath();
 //				String path=chooser.getSelectedFile().getName();
-//				System.out.println(csvFilePath);
+//				komplettArray=LadeDatei.csvRead2DArray(path);
+				System.out.println(csvFilePath);
 				musikArray=LadeDatei.csvRead(csvFilePath);
 //				System.out.println(musikArray);
 				String[][] arrayData=new String[musikArray.size()/4][4];
 				for (int i = 0; i < musikArray.size(); i++) {
 					arrayData[i/4][i%4]=musikArray.get(i);
 				}
+				
 				DefaultTableModel model=new DefaultTableModel(arrayData,columnNames);
 				jT=new JTable(model);
 				jT.setEnabled(false);
@@ -194,6 +210,12 @@ public class MainFrame extends JFrame{
 		
 	}
 	
+	/**
+	 * Edit on JTbale
+	 * if there is an update, 
+	 * change the value in the same position on the arraylist as well
+	 * 
+	 */
 	class tablecChanged_Listener implements TableModelListener{
 
 		@Override
@@ -203,7 +225,7 @@ public class MainFrame extends JFrame{
 				String newCellValue=jT.getValueAt(e.getLastRow(), e.getColumn()).toString();
 				System.out.println("new:"+newCellValue+" r:"+e.getLastRow()+" c:"+e.getColumn());
 				if(!newCellValue.equals(oldCellValue)){
-					musikArray.set(e.getLastRow()*e.getColumn()-1, newCellValue);
+					musikArray.set(e.getLastRow()*4+e.getColumn(),newCellValue);
 	
 				}
 			}
@@ -211,6 +233,9 @@ public class MainFrame extends JFrame{
 		
 	}
 	
+	/**
+	 * Track the coordinates and cell value of the last mouse click on JTable
+	 */
 	class mouseClick_Listener implements MouseListener{
 
 		@Override
@@ -247,6 +272,20 @@ public class MainFrame extends JFrame{
 		}
 		
 	}
+
+	class TextField_PlayStatus_Listener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			playStatus=(JTextField) e.getSource();
+			Musikspieler mS = new Musikspieler(playStatus.getText());
+			mS.MusikspielerSchleife();
+		}
+		
+	}
+	
+
 }
 
 
